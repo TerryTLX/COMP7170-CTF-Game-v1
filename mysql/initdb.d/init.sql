7a09cf096b49fa5435e455ff8d299abd48c9d370
TerryTLX <21469962@life.hkbu.edu.hk>
2022-04-10 20:14:27 +0800

    First commit

---
+++ mysql/initdb.d/init.sql
@@ -0,0 +1,31 @@
+SET CHARSET UTF8;
+
+DROP DATABASE IF EXISTS ctf_db;
+CREATE DATABASE ctf_db;
+USE ctf_db;
+
+DROP TABLE IF EXISTS users; 
+CREATE TABLE users (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(60) NOT NULL,
+    title VARCHAR(100) NOT NULL,
+    delete_flag BOOLEAN NOT NULL DEFAULT FALSE
+)DEFAULT CHARACTER SET=utf8;
+ 
+INSERT INTO users (full_name, title) 
+    VALUES 
+    ("Keroro", "Military Officer 軍曹"),
+    ("Giroro", "Corporal 伍長"),
+    ("Tamama", "Second Class Soldier 二等兵"),
+    ("Kururu", "Master Sergeant 曹長");
+INSERT INTO users (full_name, title, delete_flag)
+    VALUES ("Dororo", "kuu kukuku. CTF_{Invasion_Start}<br>You can find the bonus flag at another table.<br>'UNION' is useful", TRUE); /*1st Flag plus Hint*/
+
+DROP TABLE IF EXISTS flag;
+CREATE TABLE flag (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    flag VARCHAR(60) NOT NULL
+)DEFAULT CHARACTER SET=utf8;
+
+INSERT INTO flag (flag)
+    VALUES ("CTF_{Invasion_Succeed}"); /*Bonus Flag*/
---
+++ python/src/app.py
@@ -0,0 +1,62 @@
+import socketserver
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+
+import pymysql.cursors
+from jinja2 import Template, Environment, FileSystemLoader
+
+PORT = 8000
+
+class MyHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        conn = pymysql.connect(host='mysql',
+                    user='root',
+                    password='gammanebulaplanet58',
+                    db='ctf_db',
+                    charset='utf8',
+                    autocommit=True,
+                    cursorclass=pymysql.cursors.DictCursor)
+        try:
+            url = urlparse(self.path)
+
+            if url.path == '/':
+                params = parse_qs(url.query)
+                query = params.get('q', [''])[0]
+                
+                with conn.cursor() as cursor:
+                    sql = f"SELECT * FROM users WHERE delete_flag=FALSE AND title LIKE '%{query}%';"
+                    cursor.execute(sql)
+                    users = cursor.fetchall()
+
+                env = Environment(loader=FileSystemLoader('.'))
+                template = env.get_template('index.html')
+                
+                data = {
+                    'query': query,
+                    'users': users
+                }
+                disp_text = template.render(data)
+
+                self.send_response(200)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+                
+                self.wfile.write(disp_text.encode('utf-8'))
+            else:
+                self.send_response(404)
+                self.send_header('Content-type', 'text/html')
+                self.end_headers()
+        except Exception as e:
+            print(e)
+
+            self.send_response(500)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+
+            self.wfile.write(str(e).encode('utf-8'))
+        finally:
+            conn.close()
+
+with socketserver.TCPServer(("", PORT), MyHandler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
